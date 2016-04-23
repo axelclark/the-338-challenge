995bc64f075428c9a6fad76fc37c300ff2b6de72
Axel Clark <axelclark2@yahoo.com>
2016-04-22 21:53:59 -0400

    Add db seeds, header layout, and stylesheet updates

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+15.times do |n|
+  name = Faker::Team.sport
+  waiver_deadline = Faker::Time.forward(365)
+  trade_deadline = waiver_deadline + 7.days
+  championship_date = trade_deadline + 30.days
+  SportsLeague.create!(name: name,
+                       waiver_deadline: waiver_deadline,
+                       trade_deadline: trade_deadline,
+                       championship_date: championship_date,
+                      )
+end
+
+sports_leagues = SportsLeague.all
+30.times do
+  name = Faker::Team.name
+  sports_leagues.each { |league| league.fantasy_players.create!(name: name) }
+end
