5107dec8100a37aee12fab625b96c77debfd240e
Axel Clark <axelclark2@yahoo.com>
2016-04-23 14:13:43 -0400

    Add order to sports league show view

--- app/models/sports_league.rb
+++ app/models/sports_league.rb
@@ -3,4 +3,12 @@ class SportsLeague < ActiveRecord::Base
 
   validates :name, presence: true
   validates :championship_date, presence: true 
+
+  def self.by_fantasy_players
+    includes(:fantasy_players).order("fantasy_players.name")
+  end
+
+
 end
+
+
