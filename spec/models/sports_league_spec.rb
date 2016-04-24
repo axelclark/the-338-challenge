6d8662087edff1c7bdf5162c9c25fb023ad81ae6
Axel Clark <axelclark2@yahoo.com>
2016-04-24 13:21:17 -0400

    Removes N+1 Query from Sports League show view

--- spec/models/sports_league_spec.rb
+++ spec/models/sports_league_spec.rb
@@ -2,15 +2,18 @@ require 'rails_helper'
 
 RSpec.describe SportsLeague, type: :model do
   context "validations" do
+    # Associations
+    it { should have_many(:fantasy_players) }
+
+    # Validations
+    it { should validate_presence_of(:name) }
+    it { should validate_presence_of(:championship_date) }
+
     it "is valid with a name and required dates" do
+
       league = build(:sports_league)
   
       expect(league).to be_valid
     end
-   
-    # Associations
-    it { should have_many(:fantasy_players) }
-    it { should validate_presence_of(:name) }
-    it { should validate_presence_of(:championship_date) }
-  end
+  end 
 end
