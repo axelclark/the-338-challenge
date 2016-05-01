61da116a4a3c2369d92ed0b2269dc10844a43217
Axel Clark <axelclark2@yahoo.com>
2016-05-01 13:15:08 -0400

    Adds fantasy team method to caluclate points

--- app/models/fantasy_team.rb
+++ app/models/fantasy_team.rb
@@ -3,4 +3,8 @@ class FantasyTeam < ActiveRecord::Base
   has_many :fantasy_players, through: :roster_positions
 
   belongs_to :fantasy_league
+
+  def points
+    fantasy_players.joins(:final_rankings).sum(:points)
+  end
 end
--- spec/models/fantasy_team_spec.rb
+++ spec/models/fantasy_team_spec.rb
@@ -4,4 +4,18 @@ describe FantasyTeam do
   it { should have_many(:roster_positions) }
   it { should have_many(:fantasy_players).through(:roster_positions) }
   it { should belong_to(:fantasy_league) }
+  
+  describe "#points" do
+    it "returns the total points for a fantasy team" do
+      fantasy_player = create(:ranked_fantasy_player)
+      fantasy_team = create(:fantasy_team)
+      create(:roster_position, fantasy_player: fantasy_player, 
+             fantasy_team: fantasy_team)
+
+      result = fantasy_team.points 
+
+      expect(result).to eq(8) 
+    end
+  end
+
 end
