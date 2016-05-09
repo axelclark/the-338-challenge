adea8a594cd4a4429c94768fb42c0ead7f0a927c
Axel Clark <axelclark2@yahoo.com>
2016-05-08 21:09:01 -0400

    Refactor factories to add user factor form its own file and reorder the factories to standardize.

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,32 +1,24 @@
 FactoryGirl.define do
+  sequence :email do |n|
+    "user#{n}@example.com"
+  end
+
   sequence :name do |n|
     "name #{n}"
   end
-  
+
   factory :fantasy_league do
     division "A"
     year 2016
   end
 
-  factory :roster_position do
-    fantasy_player
-    fantasy_team
+  factory :fantasy_player do
+    name
+    sports_league
   end
   
   factory :fantasy_team do
     name
-  end
-  
-  factory :sports_league do
-    name
-    waiver_deadline "2016-11-11"
-    trade_deadline "2016-11-20"
-    championship_date "2017-02-01"
-  end
-
-  factory :fantasy_player do
-    name
-    sports_league
   end
   
   factory :final_ranking do
@@ -37,6 +29,27 @@ FactoryGirl.define do
       rank 1
       points 8
       winnings 25
+    end
+  end
+  
+  factory :roster_position do
+    fantasy_player
+    fantasy_team
+  end
+  
+  factory :sports_league do
+    name
+    waiver_deadline "2016-11-11"
+    trade_deadline "2016-11-20"
+    championship_date "2017-02-01"
+  end
+  
+  factory :user do
+    email
+    password "password"
+
+    factory :admin do
+      admin true
     end
   end
 end
